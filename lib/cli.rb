3c8c9091fc53203aae4f88b0535bcbed71e040d4
Jgoodrich07 <Jeromegoodrich@gmail.com>
2015-11-06 22:17:21 -0600

    refactor complete for now

--- lib/cli.rb
+++ lib/cli.rb
@@ -23,96 +23,35 @@ class CommandLineInterface
       else
         command_help
       end
+     rescue => e
+      puts e.message
     end
 
   def create_command(list)
-    if @todo.list_exist?(list)
-      list_already_exist(list)
-    else
-      @todo.create(list)
-      new_list(list)
-    end
+    @todo.create(list)
+    new_list(list)
   end
 
   def add_command(task, list)
-    if @todo.list_exist?(list)
-      @todo.add(task, list)
-      added_task(task, list)
-    else
-      list_error(list)
-    end
+    @todo.add(task, list)
+    added_task(task, list)
   end
 
   def done_command(task, list)
-    if @todo.list_exist?(list)
-      File.open("lists/#{list}.txt", "r") do |f|
-        a = f.readlines
-        a.each do |line|
-          if line.include?("[X] #{task}")
-            task_already_done(task, list)
-          elsif line.include?("#{task}")
-            @todo.done(task, list)
-            completed_task(task, list)
-          else
-            task_error(task)
-            break
-          end
-        end
-      end
-    else
-      list_error(list)
-    end
+    @todo.done(task, list)
+    completed_task(task, list)
   end
 
   def delete_command(task, list)
-    if @todo.list_exist?(list)
-      File.open("lists/#{list}.txt", "r") do |f|
-        f.readlines.each do |line|
-          if line.include?("#{task}")
-            @todo.delete(task, list)
-            deleted_task(task, list)
-          else
-            task_error(task)
-            break
-          end
-        end
-      end
-    else
-      list_error(list)
-    end
+    @todo.delete(task, list)
+    deleted_task(task, list)
   end
 
   def show_command(list)
-    if @todo.list_exist?(list)
-
-    else
-      list_error(args[1])
-    end
+    @todo.show(list)
   end
 
 private
-
-  def number_the_list(list)
-    File.open("lists/#{list}.txt", "r") do |f|
-      num = 1
-      f.each_line do |line|
-        puts "#{num}.#{line}"
-        num += 1
-      end
-    end
-  end
-
-  def list_error(list)
-    puts "sorry, the list: #{list} doesn't exist"
-  end
-
-  def list_already_exist(list)
-    puts "the list #{list} already exists"
-  end
-
-  def task_error(task)
-    puts "sorry, the task #{task} doesn't exit"
-  end
 
   def new_list(list)
     puts "created new list: #{list}"
@@ -124,10 +63,6 @@ private
 
   def completed_task(task, list)
     puts "completed task #{task} in list #{list}"
-  end
-
-  def task_already_done(task, list)
-    puts "the task #{task} in list #{list} is already done"
   end
 
   def deleted_task(task, list)
--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -2,44 +2,73 @@ class TodoList
   class ListError < StandardError
   end
 
-  def use_list(list, option, text=nil)
-    File.open("lists/#{list}.txt", option) do |f|
-      if option == "r"
-      elsif option == "w"
-      elsif option == "a+"
-        f.puts "[ ] #{text}"
-      end
-    end
-  end
-
-
   def create(list)
     if list_exist?(list)
-      raise ListError, "The list: #{list} already exists"
+      raise ListError, "The list #{list} already exists"
     else
       use_list(list,"w")
     end
   end
 
   def add(task, list)
-    use_list(list, "a+", task)
+    if list_exist?(list)
+      use_list(list, "a+", task)
+    else
+      raise ListError, "Can't add #{task} to #{list} because list #{list} doesn't exist"
+    end
   end
 
   def done(task, list)
-    if task.is_a? Numeric
-      find_task_by_number(task, list)
+    if list_exist?(list)
+      if task.to_i != 0
+        add_task_by_number(task, list)
+      else
+        add_task_by_name(task, list)
+      end
     else
-      text = File.read("lists/#{list}.txt").gsub("[ ] #{task}", "[X] #{task}")
-      File.open("lists/#{list}.txt", "w") { |f| f.puts text}
+      raise ListError, "Can't complete #{task} because list #{list} doesn't exist"
     end
   end
 
   def delete(task, list)
-    if task.is_a? Numeric
-      find_task_by_number(task, list)
+    if list_exist?(list)
+      if task.to_i != 0
+        delete_task_by_number(task, list)
+      else
+        text = delete_task(task, list)
+        use_list(list, "w", text)
+      end
     else
-      new_text_array = delete_task(task, list)
-      File.open("lists/#{list}.txt", "w") {|f| f.puts new_text_array}
+      raise ListError, "Can't delete #{task} because list #{list} doesn't exist"
+    end
+  end
+
+  def show(list)
+    if list_exist?(list)
+      a = use_list(list, "r")
+      if a.empty?
+        raise ListError, "No tasks have been added to #{list} yet"
+      else
+        a.each_with_index do |line, index|
+          puts "#{index+1}.#{line}"
+        end
+      end
+    else
+      raise ListError, "Can't show list #{list} because it doesn't exist"
+    end
+  end
+
+private
+
+  def use_list(list, option, text=nil)
+    File.open("lists/#{list}.txt", option) do |f|
+      if option == "r"
+        f.readlines
+      elsif option == "w"
+        f.puts text
+      elsif option == "a+"
+        f.puts "[ ] #{text}"
+      end
     end
   end
 
@@ -47,21 +76,56 @@ class TodoList
     File.exist?("lists/#{list}.txt")
   end
 
-  def find_task_by_number(task_number, list)
-     File.open("lists/#{list}.txt", "r") do |f|
-      a = f.readlines
-    end
-      task = a[(task_number.to_i - 1)]
-      done(task, list)
+  def convert_num_to_task(task_number, list)
+    a = use_list(list, "r")
+    task = a[(task_number.to_i - 1)]
+      if task == nil
+        raise ListError, "Task does not exist"
+      end
+    return task
   end
 
+  def add_task_by_number(task_number, list)
+      task = convert_num_to_task(task_number, list)
+      if task.include?("[X]")
+        raise ListError, "Task is already completed"
+      else
+        task.slice! "[ ] "
+        done(task, list)
+      end
+  end
 
-private
-
-def delete_task(task, list)
-    File.open("lists/#{list}.txt", "r+") do |f|
-      return f.readlines.delete_if {|line| line.include?("#{task}") }
+  def add_task_by_name(task, list)
+    a = use_list(list, "r")
+    if a.index(task) == nil
+      raise ListError, "Can't complete #{task} because it doesn't exist"
+    else
+      a.each do |line|
+        if line.include?("#{task}")
+          if line.include?("[X]")
+            raise ListError, "Task is already completed"
+          else
+            text = File.read("lists/#{list}.txt").gsub("[ ] #{task}", "[X] #{task}")
+            use_list(list, "w", text)
+            break
+          end
+        end
+      end
     end
+  end
+
+  def delete_task_by_number(task_number, list)
+    task = convert_num_to_task(task_number, list)
+    task.slice! "[ ] "
+    delete(task, list)
+  end
+
+  def delete_task(task, list)
+    a = use_list(list, "r")
+      if a.index(task) == nil
+        raise ListError, "Can't delete #{task} because it doesn't exist"
+      else
+        a.delete_if {|line| line.include?("#{task}") }
+      end
   end
 end
-
